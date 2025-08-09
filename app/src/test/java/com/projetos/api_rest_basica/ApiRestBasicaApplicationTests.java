ae4bf29b1e8c5e2493e7cfdb4f7830f51d093f4b
vitorzanela <vitor.mardig@gmail.com>
2025-08-09 18:07:49 -0300

    Inicio Projeto

---
+++ app/src/test/java/com/projetos/api_rest_basica/ApiRestBasicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos.api_rest_basica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestBasicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
